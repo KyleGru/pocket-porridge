5e083290c9fe0bb3f2aa3f78e917fff46d7e81c5
ryanjbedard <143044952+ryanjbedard@users.noreply.github.com>
2024-04-02 15:11:50 -0700

    Update Syntax in connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_UR || 'mongodb://127.0.0.1.27017/pocket-porridge');
+mongoose.connect(process.env.MONGODB_UR || 'mongodb://127.0.0.1:27017/pocket-porridge');
 
 module.exports = mongoose.connection;
