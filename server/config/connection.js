a30a7b016b5e510329d777392ebd8b3e42b49bdb
Zach Smith <zachsmith852@gmail.com>
2024-04-02 20:39:08 -0700

    update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_UR || 'mongodb://127.0.0.1:27017/pocket-porridge');
+mongoose.connect(process.env.MONGODB_UR || 'mongodb+srv://smithz852:9227@cluster0.fvh802f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
 module.exports = mongoose.connection;
