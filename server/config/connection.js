66d591922dcebd36fd75efbb050a71c8e8da5363
smithz852 <146396778+smithz852@users.noreply.github.com>
2024-04-02 22:18:30 -0700

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_UR || 'mongodb+srv://smithz852:9227@cluster0.fvh802f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+mongoose.connect(process.env.MONGODB_UR || '');
 
 module.exports = mongoose.connection;
