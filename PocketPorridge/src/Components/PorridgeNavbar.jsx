097c6d62a40b0cf19e81c32b47d12fcb42b43013
Zach Smith <zachsmith852@gmail.com>
2024-04-03 20:29:54 -0700

    Add logo

--- PocketPorridge/src/Components/PorridgeNavbar.jsx
+++ PocketPorridge/src/Components/PorridgeNavbar.jsx
@@ -3,6 +3,8 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Logo } from './Logo'
+import './PorridgeNavbar.css'
 
 export function PorridgeNavbar() {
 
@@ -10,8 +12,10 @@ export function PorridgeNavbar() {
     <>
     <header>
     <Navbar bg="primary" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Container className="logoLeft">
+          <Navbar.Brand href="#home">
+            <Logo />
+            </Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
