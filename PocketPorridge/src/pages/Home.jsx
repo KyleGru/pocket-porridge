b528a8f925d05dd7b3e4027c826f8eba61f1bedc
KyleGru <136273960+KyleGru@users.noreply.github.com>
2024-04-03 23:21:18 -0400

    Generated memes to the Home.jsx page using API

--- PocketPorridge/src/pages/Home.jsx
+++ PocketPorridge/src/pages/Home.jsx
@@ -1,5 +1,30 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
 export default function Home() {
+    const [memes, setMemes] = useState([]);
+
+    useEffect(() => {
+        const fetchMemes = async () => {
+            try {
+            const response = await axios.get('https://api.imgflip.com/get_memes')
+            const { memes } = response.data.data;
+            setMemes(memes);
+            } catch (error) {
+            console.error('Error fetching memes', error);
+            }
+        }
+        fetchMemes();
+    }, [])
+
     return (
-        <h1>Pocket Porridge</h1>
-    )
+        <div>
+            <h1>Popular Memes</h1>
+            <div>
+                {memes.map((meme, index) => (
+                <img key={index} src={meme.url} alt={meme.name} />))}
+            </div>
+        </div>
+    );
 }
