e1745bdbf939c1850eb1eb423a18125cbcd7fb8c
Zach Smith <zachsmith852@gmail.com>
2024-04-04 20:28:03 -0700

    Add modals for choosing or uploading memes

--- PocketPorridge/src/Components/CreateMemeModal.jsx
+++ PocketPorridge/src/Components/CreateMemeModal.jsx
@@ -1,5 +1,8 @@
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import { ChooseMeme } from './ChooseMeme'
+import { UploadImage } from './UploadMeme';
+import './CreateMemeModal.css'
 import React from 'react';
 
 export function CreateMemeModal(props) {
@@ -12,16 +15,14 @@ export function CreateMemeModal(props) {
     >
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
+          So You Think You're Funny, Eh?
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-        </p>
+        <div className='memeBtnFlex'>
+      <ChooseMeme />
+      <UploadImage />
+      </div>
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
