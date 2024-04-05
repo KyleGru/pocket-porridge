f75a13f0486a653b1933d7db5e1cf135c6dda08f
KyleGru <136273960+KyleGru@users.noreply.github.com>
2024-04-05 00:06:02 -0400

    Created basic modals for login and signup

--- PocketPorridge/src/Components/PorridgeNavbar.jsx
+++ PocketPorridge/src/Components/PorridgeNavbar.jsx
@@ -1,29 +1,99 @@
-import React from "react";
+import { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Logo } from './Logo'
 import './PorridgeNavbar.css'
 
 export function PorridgeNavbar() {
+const [showLoginModal, setShowLoginModal] = useState(false);
+const [showSignupModal, setShowSignupModal] = useState(false);
 
-  return (
-    <>
-    <header>
-    <Navbar bg="primary" data-bs-theme="dark">
-        <Container className="logoLeft navFlexAdjust">
-          <Navbar.Brand href="#home">
-            <Logo />
-            </Navbar.Brand>
-          <Nav className="navLinkFlex">
-            <Nav.Link href="#home">Chat</Nav.Link>
-            <Nav.Link href="#features">Search</Nav.Link>
-            <Nav.Link href="#pricing">Create Meme</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-      </header>
-    </>
-  )
+const handleLoginClick = () => setShowLoginModal(false);
+const handleLoginShow = () => setShowLoginModal(true);
+const handleSignupClick = () => setShowSignupModal(false);
+const handleSignupShow = () => setShowSignupModal(true);
+
+
+return (
+  <header>
+    <Navbar bg="primary" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">
+          <Logo />
+        </Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Chat</Nav.Link>
+          <Nav.Link href="#features">Search</Nav.Link>
+          <Nav.Link href="#pricing">Create Meme</Nav.Link>
+        </Nav>
+        <Nav className="ml-auto flex-column">
+          <Button variant="outline-light" className="mr-2" onClick={handleLoginShow}>Login</Button>
+          <Button variant="light" onClick={handleSignupShow}>Signup</Button>
+        </Nav>
+      </Container>
+    </Navbar>
+
+    <Modal show={showLoginModal} onHide={handleLoginClick}>
+      <Modal.Header closeButton>
+        <Modal.Title>Login</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Remember me" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Modal.Body>
+    </Modal>
+
+    <Modal show={showSignupModal} onHide={handleSignupClick}>
+      <Modal.Header closeButton>
+        <Modal.Title>Signup</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Remember me" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Modal.Body>
+      </Modal>
+
+  </header>
+)
 }
