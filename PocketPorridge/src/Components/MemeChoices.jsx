5f0734459b7550b1c817689778480c22e7beaab4
Zach Smith <zachsmith852@gmail.com>
2024-04-07 10:47:12 -0700

    Add function for logging specific meme ids

--- PocketPorridge/src/Components/MemeChoices.jsx
+++ PocketPorridge/src/Components/MemeChoices.jsx
@@ -13,6 +13,8 @@ export function MemeChoices() {
             try {
             const response = await axios.get('https://api.imgflip.com/get_memes')
             const { memes } = response.data.data;
+            // const { id } = memes.memes.id;
+            console.log(memes);
             setMemes(memes);
             } catch (error) {
             console.error('Error fetching memes', error);
@@ -26,11 +28,15 @@ export function MemeChoices() {
         <div className='choiceFlex'>
           {memes.map((meme, index) => (
                     <div key={index}>
-                        <button className='memeBox'>
+                        <button className='memeBox' onClick={() => {
+                           const value = meme.id
+                          console.log(value)
+                        }}>
                             <img 
                                 className='memeImg'
                                 src={meme.url}
                                 alt={meme.name}
+                                
                             />
                         </button>
                     </div>
