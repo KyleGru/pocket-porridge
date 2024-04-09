805f558ad66a086543cfab988e814fb96610c85a
ryanjbedard <143044952+ryanjbedard@users.noreply.github.com>
2024-04-08 19:56:30 -0700

    Adjust App.jsx and add graphql functionality and Apollo client functionality

--- PocketPorridge/src/App.jsx
+++ PocketPorridge/src/App.jsx
@@ -1,20 +1,44 @@
 // @TODO: Usually to rip out commented out code rather than leave commented out when committing
 // You can use git to get the code back if you really need it
-import { Outlet } from 'react-router-dom'
 import './App.css'
 import { PorridgeNavbar } from './Components/PorridgeNavbar'
-import dotenv from 'dotenv'
-dotenv.config()
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { Outlet } from 'react-router-dom'
+
+
+
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { PorridgeNavbar }) => {
+  const token = localStorage.getItem('id_token');
+
+  return {
+    PorridgeNavbar: {
+      ...PorridgeNavbar,
+        authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+3
 
 function App() {
   return (
-    <>
-    <PorridgeNavbar />
-    <Outlet/>
-    </>
-  )
+    <ApolloProvider client={client}>
+      <div>
+        <PorridgeNavbar />
+        <Outlet/>
+      </div>
+    </ApolloProvider>
+  );
 }
-
-
 
 export default App
